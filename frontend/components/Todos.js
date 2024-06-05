139d05cc699c1caff0b91e7efd0446412b33f892
Dionne <mylife33@hotmail.com>
2024-06-05 18:11:11 -0500

    MVP

--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -1,42 +1,54 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useSelector, useDispatch } from 'react-redux'
-import { toggleShowCompletedTodos } from '../state/todosSlice'
+import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleShowCompletedTodos } from "../state/todosSlice";
+import { useGetTodosQuery, useToggleTodoMutation } from "../state/todosApi";
 
 const StyledTodo = styled.li`
-  text-decoration: ${pr => pr.$complete ? 'line-through' : 'initial'};
+  text-decoration: ${(pr) => (pr.$complete ? "line-through" : "initial")};
   cursor: pointer;
-`
+`;
 
 export default function Todo() {
+  // query
+  const { data: todos } = useGetTodosQuery();
+  const [toggleTodo] = useToggleTodoMutation();
   // redux
-  const showCompletedTodos = useSelector(st => st.todosState.showCompletedTodos)
-  const dispatch = useDispatch()
+  const showCompletedTodos = useSelector(
+    (st) => st.todosState.showCompletedTodos
+  );
+  const dispatch = useDispatch();
   return (
     <div id="todos">
       <div className="error"></div>
       <h3>Todos</h3>
       <ul>
-        {
-          [
-            { id: 1, label: 'Laundry', complete: true },
-            { id: 2, label: 'Groceries', complete: false },
-            { id: 3, label: 'Dishes', complete: false },
-          ].filter(todo => {
-            return showCompletedTodos || !todo.complete
-          })
-            .map(todo => {
-              return (
-                <StyledTodo $complete={todo.complete} key={todo.id}>
-                  <span>{todo.label}{todo.complete && ' ✔️'}</span>
-                </StyledTodo>
-              )
+        {todos &&
+          todos
+            .filter((todo) => {
+              return showCompletedTodos || !todo.complete;
             })
-        }
+            .map((todo) => {
+              const onToggle = () => {
+                toggleTodo({ id: todo.id, todo: { complete: !todo.complete } });
+              };
+              return (
+                <StyledTodo
+                  onClick={onToggle}
+                  $complete={todo.complete}
+                  key={todo.id}
+                >
+                  <span>
+                    {todo.label}
+                    {todo.complete && " ✔️"}
+                  </span>
+                </StyledTodo>
+              );
+            })}
       </ul>
       <button onClick={() => dispatch(toggleShowCompletedTodos())}>
-        {showCompletedTodos ? 'Hide' : 'Show'} completed todos
+        {showCompletedTodos ? "Hide" : "Show"} completed todos
       </button>
     </div>
-  )
+  );
 }
